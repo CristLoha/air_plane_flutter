30faf1d889b516adf3d9afc048d8146baaf3c844
ChristLoha <cristloha2205@gmail.com>
2021-08-02 17:50:17 +0800

    update

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:air_plane/shared/theme.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatelessWidget {
@@ -11,12 +12,34 @@ class MainPage extends StatelessWidget {
         child: Container(
           width: double.infinity,
           height: 60,
-          color: Colors.red,
+          margin: EdgeInsets.only(bottom: 30, left: defaultMargin),
+          decoration: BoxDecoration(
+            color: kWhiteColor,
+            borderRadius: BorderRadius.circular(18),
+          ),
+          child: Row(
+            children: [
+              Column(
+                children: [
+                  Container(
+                      width: 24,
+                      height: 24,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                        image: AssetImage(
+                          'assets/icon_home.png',
+                        ),
+                      )))
+                ],
+              )
+            ],
+          ),
         ),
       );
     }
 
     return Scaffold(
+      backgroundColor: kBackgroundColor,
       body: Stack(
         children: [
           customBottomNavigation(),
