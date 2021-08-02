d81970a6507fd83d969b55ba0b6f7af4f63bbc01
ChristLoha <cristloha2205@gmail.com>
2021-08-02 17:42:13 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:air_plane/ui/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'ui/pages/bonus_page.dart';
 import 'ui/pages/get_started.dart';
+import 'ui/pages/main_page.dart';
 import 'ui/pages/sign_up_page.dart';
 
 void main() => runApp(MyApp());
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
         '/get-started': (context) => GetStartedPage(),
         '/sign-up': (context) => SignUpPage(),
         '/bonus': (context) => BonusPage(),
+        '/main': (context) => MainPage(),
       },
     );
   }
---
+++ lib/ui/pages/main_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget customBottomNavigation() {
+      return Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          width: double.infinity,
+          height: 60,
+          color: Colors.red,
+        ),
+      );
+    }
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          customBottomNavigation(),
+        ],
+      ),
+    );
+  }
+}
