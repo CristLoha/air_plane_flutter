aa6c359206e1abcb9ea37c256c7a80359e41595b
ChristLoha <cristloha2205@gmail.com>
2021-08-10 15:44:19 +0800

    update

--- lib/ui/pages/checkout_page.dart
+++ lib/ui/pages/checkout_page.dart
@@ -1,5 +1,7 @@
 import 'package:air_plane/shared/theme.dart';
+import 'package:air_plane/ui/pages/booking_item_details.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class CheckouPage extends StatelessWidget {
   const CheckouPage({Key? key}) : super(key: key);
@@ -71,6 +73,144 @@ class CheckouPage extends StatelessWidget {
       );
     }
 
+    Widget bookingDetails() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 30,
+        ),
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 30,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(18),
+          color: kWhiteColor,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                /// NOTE: DESTINATION TILE
+                Container(
+                  width: 70,
+                  height: 70,
+                  margin: EdgeInsets.only(right: 16),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(
+                        'assets/image_destination1.png',
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Lake Ciliwung',
+                        style: blackTextStyle.copyWith(
+                          fontSize: 18,
+                          fontWeight: medium,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        'Tanggerang',
+                        style: greyTextStyle.copyWith(
+                          fontWeight: light,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 20,
+                      height: 24,
+                      margin: EdgeInsets.only(
+                        right: 2,
+                      ),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                            'assets/icon_star.png',
+                          ),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '4.8',
+                      style: blackTextStyle.copyWith(
+                        fontWeight: medium,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+
+            /// NOTE: BOOKING DETAILS TEXT
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: Text(
+                'Booking Details',
+                style: blackTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: semiBold,
+                ),
+              ),
+            ),
+
+            /// NOTE: BOOKING DETAILS ITEM
+            BookingDetailsitems(
+              title: 'Seat',
+              valueText: 'A3 B3',
+              valueColor: kBlackColor,
+            ),
+
+            BookingDetailsitems(
+              title: 'Insurance',
+              valueText: 'YES',
+              valueColor: kGreenColor,
+            ),
+
+            BookingDetailsitems(
+              title: 'Refundable',
+              valueText: 'NO',
+              valueColor: kRedColor,
+            ),
+
+            BookingDetailsitems(
+              title: 'VAT',
+              valueText: '45%',
+              valueColor: kBlackColor,
+            ),
+
+            BookingDetailsitems(
+              title: 'Price',
+              valueText: 'IDR 8.500.690',
+              valueColor: kRedColor,
+            ),
+
+            BookingDetailsitems(
+              title: 'Grand Total',
+              valueText: 'IDR 12.000.000',
+              valueColor: kPrimaryColor,
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: ListView(
@@ -79,6 +219,7 @@ class CheckouPage extends StatelessWidget {
         ),
         children: [
           route(),
+          bookingDetails(),
         ],
       ),
     );
